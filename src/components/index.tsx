d298c6a936f334b5bb73b947cd8b53bc48d0967c
alitaso345 <alice.maru345@gmail.com>
2020-04-25 19:08:49 +0900

    add rippleButton

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import styled from 'styled-components'
 import RippleButton from './rippleButton'
 
 type Props = { className?: string }
@@ -10,13 +11,35 @@ const View: React.FC<Props> = props => (
       height={'64px'}
       borderRadius={'10px'}
       backgroundColor={'#7089b9'}
-      effectSize={200}
+      effectSize={280}
     >
       <span className="children">
-        UPLOAD
+        DOWNLOAD
       </span>
     </RippleButton>
   </div>
 )
 
-export default View
+export default styled(View)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > * {
+    margin: 10px;
+    > .children {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      > * {
+        margin-right: 10px;
+      }
+    }
+  }
+`
