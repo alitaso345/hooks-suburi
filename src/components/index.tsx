14ecd177025ea231dbe8a2731ea69b4ffe5ac159
alitaso345 <alice.maru345@gmail.com>
2020-04-29 00:27:37 +0900

    add records

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,34 @@
 import * as React from 'react'
+import styled from 'styled-components'
+import { records } from './records'
 
-const View: React.FC = () => (
-  <div>hello</div>
+type Props = { className?: string }
+
+const View: React.FC<Props> = props => (
+  <div className={props.className}>
+    {records.map((record, index) => (
+      <div key={index} className="item">
+        <span>{record.title}</span>
+      </div>
+    ))}
+  </div>
 )
 
-export default View
+export default styled(View)`
+  margin: 16px;
+  background-color: #fff;
+  border-radius: 3px;
+  overflow: hidden;
+  box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.2);
+  > .item {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+    border-bottom: 1px solid #eee;
+    &:last-child {
+      border: none;
+    }
+  }
+`
